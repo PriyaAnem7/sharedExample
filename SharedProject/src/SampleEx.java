fbfd7c0bb6d71cbb76c91f3acb5c00417e986c2d
PriyaAnem <anempriya57@gmail.com>
2021-02-08 21:51:38 +0530

    Commit

---
+++ SharedProject/src/SampleEx.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class SampleEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			int a, b, c, d;
+	        Scanner s = new Scanner(System.in);
+	        System.out.println("Enter all three numbers:");
+	        a = s.nextInt();
+	        b = s.nextInt();
+	        c = s.nextInt();
+	        d = c > (a > b ? a : b) ? c : ((a > b) ? a : b);
+	        System.out.println("Largest Number:"+d);
+
+	}
+
+}
